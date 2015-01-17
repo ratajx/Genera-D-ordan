4b22c1e689fccc2d21f45e8086e297c8f8683703
ratajx <ratajx93@gmail.com>
2015-01-17 04:04:26 +0100

    nm

--- General/Form1.Designer.cs
+++ General/Form1.Designer.cs
@@ -61,7 +61,7 @@
             this.kodPocztowyDataGridViewTextBoxColumn,
             this.miastoDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.bazyBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(21, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(90, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(534, 150);
             this.dataGridView1.TabIndex = 0;
